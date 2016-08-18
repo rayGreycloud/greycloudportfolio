3e79104a8d9bc4a77f1ece6883a32a5bae3a8690
rayGreycloud <rayGreycloud@gmail.com>
2016-08-18 13:11:30 -0700

    Add icon support and refactor contact section

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -4,19 +4,26 @@ import {Link} from 'react-router';
 const Contact = () => {
   return (
     <div>
-      <h1 className='text-center page-title'>Contact Me</h1>
-      <p>Let's talk about what I can do for you:</p>
+      <h2 className='text-center page-title'>Contact Me</h2>
+      <p>Let's talk about what I can do for you. Send me a message, and we'll set up a time to chat.</p>
       <div className="section" id="contact">
         <div className="content">
-          <h2>Contact Me</h2>
-          <div>
-           <a title="Email Me" href="mailto:rayGreycloud@gmail.com" target="_blank" class="btn btn-primary btn-lg"><i class="fa fa-envelope-o"></i><span> Email Me</span></a>
 
-           <a title="Tweet Me" href="https://twitter.com/rayGreycloud" target="_blank" class="btn btn-primary btn-lg">
-             <i class="fa fa-twitter-square"></i>
-             <span>Tweet Me</span>
-            </a>
-          </div>
+          <ul className="social_links">
+            <li className="email">
+              <a title="Email Me" href="mailto:rayGreycloud@gmail.com" target="_blank" className="btn btn-primary btn-lg">
+                <i className="fa fa-envelope-o"></i>
+                <span> Email Me</span>
+              </a>
+            </li>
+
+            <li>
+              <a title="Tweet Me" href="https://twitter.com/rayGreycloud" target="_blank" className="btn btn-primary btn-lg">
+                <i className="fa fa-twitter-square"></i>
+                <span>Tweet Me</span>
+              </a>
+            </li>
+            </ul>
 
         </div>
       </div>
