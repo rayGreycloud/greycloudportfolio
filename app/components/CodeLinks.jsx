0eaaa7ae302c258a01bb934e99bbe51c98801a3c
rayGreycloud <rayGreycloud@gmail.com>
2016-08-19 15:31:00 -0700

    Revise CodeLinks button styling

--- app/components/CodeLinks.jsx
+++ app/components/CodeLinks.jsx
@@ -2,25 +2,24 @@ import React from 'react';
 
 const CodeLinks = () => {
   return (
-    <div className="code-links">
-      <h3><i className="fa fa-code"></i> More of My Code:</h3>
-      <ul>
-
+    <div id="code-links">
+      <h3 className="section-title"><i className="fa fa-code"></i> More of My Code:</h3>
+      <ul className="code_links">
         <li>
-          <a title="@raygreycloud's GitHub Profile" href="https://github.com/rayGreycloud" target="_blank" className="btn btn-primary btn-lg">
+          <a title="@raygreycloud's GitHub Profile" href="https://github.com/rayGreycloud" target="_blank" className="button hollow primary">
             <i className="fa fa-github-square"></i>
             <span>My GitHub Profile</span>
           </a>
         </li>
 
         <li>
-          <a title="raygreycloud's code portfolio" href="http://www.freecodecamp.com/raygreycloud" target="_blank" className="btn btn-primary btn-lg">
+          <a title="raygreycloud's code portfolio" href="http://www.freecodecamp.com/raygreycloud" target="_blank" className="button hollow primary">
             <span>My freeCodeCamp(<i className="fa fa-fire"></i>) portfolio</span>
           </a>
         </li>
 
         <li>
-          <a title="Ray Greycloud's CodePen" href="http://codepen.io/rayGreycloud" target="_blank" className="btn btn-primary btn-lg">
+          <a title="Ray Greycloud's CodePen" href="http://codepen.io/rayGreycloud" target="_blank" className="button hollow primary">
               <i className="fa fa-codepen"></i>
               <span> My CodePen Portfolio</span>
           </a>
