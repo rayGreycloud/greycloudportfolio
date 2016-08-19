fc99c6b3f3289794379d5c3a37da61acca7e3b23
rayGreycloud <rayGreycloud@gmail.com>
2016-08-19 15:17:44 -0700

    Revise text and styling

--- app/components/Splash.jsx
+++ app/components/Splash.jsx
@@ -10,16 +10,21 @@ const Splash = (props) => {
           <h5><em> --Benjamin Franklin</em></h5>
         </div>
         <div>
-          <h3>rayGreycloud.webDev()</h3>
-          <p>
-            I'm Ray, a web developer in Portland. I believe diligence and attention to detail are essential for good web app development. Especially going forward as the technologies and toolchains become more complicated and interconnected.
-          </p>
+          <h3>rayGreycloud.webDev</h3>
+          <div className="opening">
+            <p>
+              I'm Ray, a web developer in Portland. I believe diligence and attention to detail are essential for good web app development. Especially going forward as the technologies and toolchains become more complicated and interconnected.
+            </p>
+            <p>
+              Click the links to learn more about myself, my projects or how to contact me.
+            </p>
           <p>
             <a className="button hollow primary" href="#about">More about me</a>
           </p>
         </div>
       </div>
     </div>
+  </div>
   )
 };
 
