fa64d75572010417dbdd7646890606bea04c9dd7
rayGreycloud <rayGreycloud@gmail.com>
2016-08-19 14:24:27 -0700

    Refactor Contact to simplify structure and revise button styling

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -3,34 +3,38 @@ import {Link} from 'react-router';
 
 const Contact = () => {
   return (
-    <div>
-      <h2 className='text-center page-title'>Contact Me</h2>
-      <p>Let's talk about what I can do for you. Send me a message, and we'll set up a time to chat.</p>
-      <div className="section" id="contact">
-        <div className="content">
+    <div id="contact">
 
-          <ul className="social_links">
-            <li className="email">
-              <a title="Email Me" href="mailto:rayGreycloud@gmail.com" target="_blank" className="btn btn-primary btn-lg">
-                <button className="button hollow primary">
-                  <i className="fa fa-envelope-o"></i>
-                  <span> Email Me</span>
-                </button>
-              </a>
-            </li>
-
-            <li>
-              <a title="Tweet Me" href="https://twitter.com/rayGreycloud" target="_blank" className="btn btn-primary btn-lg">
-                <button className="button hollow primary">
-                <i className="fa fa-twitter-square"></i>
-                <span> Tweet Me</span>
-                </button>
-              </a>
-            </li>
-            </ul>
-
-        </div>
+      <div>
+        <h2 className='text-center page-title'>
+          Contact Me
+        </h2>
+        <p>Let's talk about what I can do for you. Send me a message, and we'll set up a time to chat.</p>
       </div>
+
+      <div className="content">
+        <ul className="social_links">
+
+          <li className="email">
+            <a title="Email Me"
+              className="button hollow primary" href="mailto:rayGreycloud@gmail.com" target="_blank">
+              <i className="fa fa-envelope-o"></i>
+              <span> Email Me</span>
+            </a>
+          </li>
+
+          <li>
+            <a title="Tweet Me"
+              className="button hollow primary"
+              href="https://twitter.com/rayGreycloud" target="_blank">
+              <i className="fa fa-twitter-square"></i>
+              <span> Tweet Me</span>
+            </a>
+          </li>
+
+        </ul>
+      </div>
+
     </div>
   );
 };
