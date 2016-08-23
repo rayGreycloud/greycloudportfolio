58376fc2274e7ec3956ce9cc00a2b68bb27cb942
rayGreycloud <rayGreycloud@gmail.com>
2016-08-23 14:36:46 -0700

    Add more details to projectItem

--- app/components/ProjectList.jsx
+++ app/components/ProjectList.jsx
@@ -6,15 +6,15 @@ const ProjectList = (props) => {
   const projectItems = projectsData.map((project) => {
       return (
         <div key={project.name} className="col-md-6 list-group-item">
-          <a href={project.codeURL}>
-            <div className="thumbnail">
-              <img src={project.imageURL}></img>
-              <div className="caption">
-                <h4>{project.name}</h4>
-                <p>{project.re}</p>
-              </div>
+          <div className="thumbnail">
+            <img src={project.imageURL}></img>
+            <div className="caption">
+              <h4>{project.name}</h4>
+              <p>{project.re}</p>
+              <a href={project.appURL}>Live App</a>
+              <a href={project.codeURL}>GitHub Repo</a>
             </div>
-          </a>
+          </div>
         </div>
       );
   });
