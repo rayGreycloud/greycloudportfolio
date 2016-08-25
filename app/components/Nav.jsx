ea99ec2f55b1e85b2ce2dab679a3e5f1bd89caf7
rayGreycloud <rayGreycloud@gmail.com>
2016-08-25 13:04:17 -0700

    Refactor nav to correct styling

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,25 +2,32 @@ import React from 'react';
 import {Link, IndexLink} from 'react-router';
 
 const Nav = () => {
+
   return (
-    <div className='top-bar'>
-      <div className='top-bar-left'>
-        <ul className='menu'>
-          <li className='menu-text'>rayGreycloud.xyz</li>
-          <li>
-            <IndexLink to='/' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Home</IndexLink>
-          </li>
-          <li>
-            <Link to='/about' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>About</Link>
-          </li>
-          <li>
-            <Link to='/projects' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Projects</Link>
-          </li>
-          <li>
-            <Link to='/contact' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Contact</Link>
-          </li>
-        </ul>
+    <div className="navbar">
+
+
+
+      <div className="top-bar" id="responsive-menu">
+        <div className="top-bar-left">
+          <ul className="menu ">
+            <li className="menu-text">rayGreycloud.xyz</li>
+            <li className="menu-text">
+              <IndexLink to='/' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Home</IndexLink>
+            </li>
+            <li>
+              <Link to='/about' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to='/projects' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Projects</Link>
+            </li>
+            <li>
+              <Link to='/contact' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Contact</Link>
+            </li>
+          </ul>
+        </div>
       </div>
+
     </div>
   );
 }
