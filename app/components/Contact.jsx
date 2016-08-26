75d7ad085be5060a1d5ff4eb64cdf5539afede96
rayGreycloud <rayGreycloud@gmail.com>
2016-08-26 12:22:32 -0700

    Change button classes back to foundation

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
 
           <li className="email">
             <a title="Email Me"
-              className="btn btn-default" href="mailto:rayGreycloud@gmail.com" target="_blank">
+              className="button hollow primary" href="mailto:rayGreycloud@gmail.com" target="_blank">
               <i className="fa fa-envelope-o"></i>
               <span> Email Me</span>
             </a>
@@ -25,7 +25,7 @@ const Contact = () => {
 
           <li>
             <a title="Tweet Me"
-              className="btn btn-default"
+              className="button hollow primary"
               href="https://twitter.com/rayGreycloud" target="_blank">
               <i className="fa fa-twitter-square"></i>
               <span> Tweet Me</span>
