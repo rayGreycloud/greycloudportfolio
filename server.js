fb4fd3eb06c488de72fd506740369afa4ba22305
rayGreycloud <rayGreycloud@gmail.com>
2016-08-30 21:22:42 -0700

    Add favicon server

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const favicon = require('serve-favicon');
 const PORT = process.env.PORT || 3000;
 
-
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static('public'));
 
 // For use with browserHistory in app.jsx
